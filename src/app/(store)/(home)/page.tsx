2bcf40748f4b46f33c812bccdfe110570e55e2f0
eualbino <lucas.g.apostolo@gmaul.com>
2024-01-09 12:37:19 -0300

    baseUrl and cache

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,5 +1,18 @@
+import { api } from "@/app/data/api";
 import Image from "next/image";
 import Link from "next/link";
+
+async function getFeaturedProducts(): Promise<Product[]> {
+	const response = await api('/products/featured', {
+		next: {
+			revalidate: 60 * 60,
+		},
+		
+		// cache: 'force-cache' / 'no-store'
+	})
+	const products = await response.json()
+	return products
+}
 
 export default async function Home() {
 	return (
