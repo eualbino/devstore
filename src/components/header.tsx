8c2c97c467d70e07d7c6d701ad9337e31bd37a65
eualbino <lucas.g.apostolo@gmaul.com>
2024-01-09 01:35:14 -0300

    Page Home and API with Next Fetch Api

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,7 +28,7 @@ export function Header() {
 				<Link href="/" className="flex items-center gap-2 hover:underline">
 					<span className="text-sm">Account</span>
 					<Image
-						src="https://github.com/rickxz.png"
+						src="https://github.com/leonardoofattori.png"
 						className="h-6 w-6 rounded-full"
 						width={24}
 						height={24}
